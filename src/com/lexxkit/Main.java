98709396995c30d5a2cfc09821e6b33b98a11e57
Lexxkit <lexxkit@gmail.com>
2022-06-06 16:35:47 +0300

    added task 1

--- src/com/lexxkit/Main.java
+++ src/com/lexxkit/Main.java
@@ -1,8 +1,22 @@
 package com.lexxkit;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        printOddNumbers(nums);
     }
+
+    private static void printOddNumbers(List<Integer> nums) {
+        for (Integer num : nums) {
+            if (num % 2 != 0 ) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+
+
 }
